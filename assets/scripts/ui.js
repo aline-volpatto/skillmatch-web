a04f14960546907a26da07add3518b378db939f1
Aline Volpatto <acvolpatto@gmail.com>
2026-07-08 20:05:47 -0300

    Cria formulário com captura, validacao e preventDefault

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -9,3 +9,38 @@ export function limparStatus (){ //Limpa a mensagem de status, quando as vagas c
     const status = document.getElementById("status");
     status.textContent = "";
 }
+
+//Pega o formulário do html, fica disponível para preencimento, envia o formulário e previne de recarregar a página.
+export function formulario(){
+    const form = document.getElementById("form-perfil");
+    const erro = document.getElementById("form-erro");
+
+    form.addEventListener("submit", function (evento){
+        evento.preventDefault();
+
+        //pega os dados digitados no formulário.
+        const nome = document.getElementById("nome").value;
+        const area = document.getElementById("area").value;
+        const habilidadesTexto = document.getElementById("habilidades").value;
+        const experiencia = document.getElementById("experiencia").value;
+
+        if(nome === "" || area === "" || habilidadesTexto === ""){ //Caso nome, área ou experiencia estejam em branco, retorna mensagem de erro.
+            erro.textContent = "Por gentileza, preencha todos os campos."
+            return;
+        }
+        erro.textContent = ""; //Se algo estava escrito, limpa o erro anterior.
+
+        //Transforma o conteúdo digitado entre vírgulas, em um novo array.
+        const habilidades = habilidadesTexto.split(",").map(habilidade => habilidade.trim()); //trim: caso o usuário digite espaço, o trim apaga o espaço e a mensagem fica vazia. Evita burlar a validação usando espaços.
+
+        //Monta o objeto do candidato (com todas informações)
+        const candidato = {
+            nome: nome,
+            area: area,
+            habilidades: habilidades,
+            experienciaMeses: Number(experiencia)
+        };
+
+        console.log("Candidato", candidato);
+    });
+}
