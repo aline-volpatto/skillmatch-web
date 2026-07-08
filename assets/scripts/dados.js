1b2e08c8c878ad269513a73cc4d605856a806c8c
Aline Volpatto <acvolpatto@gmail.com>
2026-07-08 16:56:22 -0300

    Transforma json em objetos Vaga com map.

--- assets/scripts/dados.js
+++ assets/scripts/dados.js
@@ -1,3 +1,5 @@
+import {Vaga} from "./motor.js"; //Busca a classe Vaga.
+
 export async function carregarVagas(){ //Nessa função, busco as vagas do arquivo json de forma assíncrona. Retorna um array de vagas convertido para o main.js usar.
     const resposta = await fetch("./assets/dados/vagas.json"); //Aqui espera o arquivo chegar.
     
@@ -5,8 +7,17 @@ export async function carregarVagas(){ //Nessa função, busco as vagas do arqui
         throw new Error("Não foi possível carregar as vagas (status" + resposta.status + ")");
     }
     
-    const vagas = await resposta.json(); //Aqui converte o texto em um array.
-    return vagas; //devolve as vagas prontas para leitura.
+    const dados = await resposta.json(); //Aqui converte o texto em um array.
+    const vagas = dados.map(dadosVaga => new Vaga( //transforma cada objeto cru (json) em um novo objeto Vaga.
+        dadosVaga.id,
+        dadosVaga.empresa,
+        dadosVaga.cargo,
+        dadosVaga.requisitos,
+        dadosVaga.salario,
+        dadosVaga.modalidade,
+    ));
+
+    return vagas; //devolve instancias das vagas prontas.
 }
 
 
